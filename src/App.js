e151fdfa46c68241dc57196a0d94447b823c9517
sameerthecoder94 <sameerthecoder@gmail.com>
2022-01-21 18:42:45 -0500

    http request(using axios) and updating state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 // import UserItem from './components/users/UserItem';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  // componentDidMount() {
+  //   axios.get('https://api.github.com/users').then((res) => {
+  //     console.log(res.data);
+  //   });
+  // }
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <Fragment>
         <Navbar title='Github Finder' icon='fab fa-github' />
         {/* <UserItem /> */}
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </Fragment>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,33 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        login_name: 'mojombo',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        id: '2',
-        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-        login_name: 'defunkt',
-        html_url: 'https://github.com/defunkt',
-      },
-      {
-        id: '3',
-        avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-        login_name: 'pjhyett',
-        html_url: 'https://github.com/pjhyett',
-      },
-    ],
-  };
-
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {this.props.users.map((user) => (
           <UserItem key={user.id} userObj={user} />
         ))}
       </div>
