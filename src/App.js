3525c3613eccbbc735c81bc7ef6838be32dacda1
sameerthecoder94 <sameerthecoder@gmail.com>
2022-01-22 11:08:30 -0500

    Alert State and component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
   // componentDidMount() {
   //   axios.get('https://api.github.com/users').then((res) => {
@@ -41,21 +43,28 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  //Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
+
   render() {
     const { users, loading } = this.state;
     return (
-      <Fragment>
-        <Navbar title='Github Finder' icon='fab fa-github' />
-        <Search
-          searchUsers={this.searchUsers}
-          clearUsers={this.clearUsers}
-          showClear={users.length > 0 ? true : false}
-        />
-        {/* <UserItem /> */}
+      <div className='App'>
+        <Navbar />
         <div className='container'>
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
           <Users loading={loading} users={users} />
         </div>
-      </Fragment>
+      </div>
     );
   }
 }
