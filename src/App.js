b925044a894dcec50890df555fc9c92036bda64c
sameerthecoder94 <sameerthecoder@gmail.com>
2022-01-22 09:28:27 -0500

    passing Props Up and Search

--- src/App.js
+++ src/App.js
@@ -16,21 +16,31 @@ class App extends Component {
   //     console.log(res.data);
   //   });
   // }
+
   async componentDidMount() {
     this.setState({ loading: true });
-
     const res = await axios.get(
-      `https://api.github.com/users?client_id={process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret={process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
     this.setState({ users: res.data, loading: false });
   }
 
+  //Search Github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
   render() {
     return (
       <Fragment>
         <Navbar title='Github Finder' icon='fab fa-github' />
-        <Search />
+        <Search searchUsers={this.searchUsers} />
         {/* <UserItem /> */}
         <div className='container'>
           <Users loading={this.state.loading} users={this.state.users} />
